e56ae5bc1ea177abff994521da4d008c1c459328
Nikolai <kolyamn@gmail.com>
2023-12-18 18:26:24 +0300

    Solved

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -1,6 +1,10 @@
 package ru.kata.spring.boot_security.demo.service;
 
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 
-public interface RoleService extends RoleRepository {
+import java.util.List;
+
+public interface RoleService {
+    List<Role> getAllRoles();
 }
